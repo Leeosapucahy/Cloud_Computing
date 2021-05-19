71d7af8d02eece4357b2fcfa29b3c92e76ed3669
Leosapucahy <69973081+Leosapucahy@users.noreply.github.com>
2021-05-19 18:49:43 -0300

    Add files via upload
    network atualizado

--- Desafio 2/terraform-aws/network.tf
+++ Desafio 2/terraform-aws/network.tf
@@ -6,20 +6,14 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+####################################################################
+
+# A
+
 resource "aws_subnet" "public_a" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "mp_leo"
-  }
-}
-
-resource "aws_subnet" "public_c" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1c"
 
   tags = {
     Name = "mp_leo"
@@ -36,9 +30,139 @@ resource "aws_subnet" "private_app_a" {
   }
 }
 
-resource "aws_subnet" "private_app_c" {
+resource "aws_subnet" "private_db_a" {
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.5.0/24"
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "mp_leo"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id = aws_subnet.public_a.id
+
+  route_table_id = aws_route_table.router_public.id
+
+}
+
+
+resource "aws_route_table_association" "private_app_a" {
+  subnet_id = aws_subnet.private_app_a.id
+
+  route_table_id = aws_route_table.router_private_a.id
+
+}
+
+resource "aws_route_table_association" "private_db_a" {
+  subnet_id = aws_subnet.private_db_a.id
+
+  route_table_id = aws_route_table.router_private_a.id
+
+}
+
+##########################################################################
+
+# REDES
+
+resource "aws_eip" "nat" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "mp_leo"
+  }
+
+}
+
+resource "aws_eip" "nat2" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "mp_leo"
+  }
+
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "mp_leo"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw_a" {
+  allocation_id = aws_eip.nat.id #ELASTIC IP 1
+  subnet_id     = aws_subnet.public_a.id
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "mp_leo"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw_c" {
+  allocation_id = aws_eip.nat2.id #ELASTIC IP 2
+  subnet_id     = aws_subnet.public_c.id
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "mp_leo"
+  }
+}
+
+# ROTA PÚBLICA
+resource "aws_route_table" "router_public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "mp_leo"
+  }
+}
+
+# ROTA PRIVADA
+resource "aws_route_table" "router_private_a" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw_a.id
+  }
+
+  tags = {
+    Name = "mp_leo"
+  }
+}
+
+resource "aws_route_table" "router_private_c" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw_c.id
+  }
+
+  tags = {
+    Name = "mp_leo"
+  }
+}
+#####################################################################
+
+
+# C
+
+resource "aws_subnet" "public_c" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1c"
 
   tags = {
@@ -46,10 +170,10 @@ resource "aws_subnet" "private_app_c" {
   }
 }
 
-resource "aws_subnet" "private_db_a" {
+resource "aws_subnet" "private_app_c" {
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.6.0/24"
-  availability_zone = "us-east-1a"
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "us-east-1c"
 
   tags = {
     Name = "mp_leo"
@@ -66,60 +190,23 @@ resource "aws_subnet" "private_db_c" {
   }
 }
 
-resource "aws_eip" "nat" {
-  vpc        = true
-  depends_on = [aws_internet_gateway.igw]
+resource "aws_route_table_association" "public_c" {
+  subnet_id = aws_subnet.public_c.id
 
-  tags = {
-    Name = "mp_leo"
-  }
+  route_table_id = aws_route_table.router_public.id
 
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_route_table_association" "private_app_c" {
+  subnet_id = aws_subnet.private_app_c.id
 
-  tags = {
-    Name = "mp_leo"
-  }
-}
-
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.private_app_a.id
-
-  depends_on = [aws_internet_gateway.igw]
-
-  tags = {
-    Name = "mp_leo"
-  }
+  route_table_id = aws_route_table.router_private_c.id
 
 }
 
-resource "aws_route_table" "router" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_route_table_association" "private_db_c" {
+  subnet_id = aws_subnet.private_db_c.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw.id
-  }
-
-  tags = {
-    Name = "mp_leo"
-  }
-
-}
-
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.router.id
-
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id = aws_subnet.private_app_a.id
-   
-  route_table_id = aws_route_table.router.id
+  route_table_id = aws_route_table.router_private_c.id
 
 }
