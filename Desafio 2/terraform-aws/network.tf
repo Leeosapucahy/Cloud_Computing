6e68f1ec34309f3501d80b328a9b88a785f6fd62
Leosapucahy <69973081+Leosapucahy@users.noreply.github.com>
2021-05-24 17:57:38 -0300

    Add files via upload

--- Desafio 2/terraform-aws/network.tf
+++ Desafio 2/terraform-aws/network.tf
@@ -132,7 +132,7 @@ resource "aws_route_table_association" "private_db_c" {
 
 # REDES
 
-#ELASTIC IP 1(A)
+# ELASTIC IP 1(A)
 resource "aws_eip" "nat" {
   vpc        = true
   depends_on = [aws_internet_gateway.igw]
@@ -143,7 +143,7 @@ resource "aws_eip" "nat" {
 
 }
 
-#ELASTIC IP 2 (C)
+# ELASTIC IP 2 (C)
 resource "aws_eip" "nat2" {
   vpc        = true
   depends_on = [aws_internet_gateway.igw]
@@ -154,7 +154,7 @@ resource "aws_eip" "nat2" {
 
 }
 
-#INTERNET GATEWAY
+# INTERNET GATEWAY
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
   tags = {
@@ -162,7 +162,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-#ELASTIC IP A
+# NATGATEWAY A
 resource "aws_nat_gateway" "nat_gw_a" {
   allocation_id = aws_eip.nat.id 
   subnet_id     = aws_subnet.public_a.id
@@ -174,7 +174,7 @@ resource "aws_nat_gateway" "nat_gw_a" {
   }
 }
 
-#ELASTIC IP C
+# NATGATEWAY C
 resource "aws_nat_gateway" "nat_gw_c" {
   allocation_id = aws_eip.nat2.id 
   subnet_id     = aws_subnet.public_c.id
@@ -214,7 +214,7 @@ resource "aws_route_table" "router_private_a" {
   }
 }
 
-#ROTA PRIVADA C DEFINIDA
+# ROTA PRIVADA C DEFINIDA
 resource "aws_route_table" "router_private_c" {
   vpc_id = aws_vpc.vpc.id
 
@@ -227,3 +227,102 @@ resource "aws_route_table" "router_private_c" {
     Name = "mp_leo"
   }
 }
+
+###############################################################################################################
+
+# AUTO SCALING LAUNCH CONFIGURATION
+resource "aws_launch_configuration" "aws_launch" {
+  name = "custom-launch-config"
+  image_id = ""
+  instance_type = "t2.micro"
+  key_name = "terraform-aws-mp"
+}
+
+# AUTO SCALING GROUP
+resource "aws_autoscaling_group" "aws_group" {
+  name                      = "aws_group"
+  max_size                  = 3
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  force_delete              = true
+  launch_configuration      = aws_launch_configuration.aws_launch.name
+  vpc_zone_identifier       = ["${aws_security_group.acesso-ssh-mp.id}"]
+  tag {
+    key = "name"
+    value = "ec2_desafio"
+    propagate_at_launch = true
+  }
+}
+
+# AUTO SCALING CONFIGURATION POLICY
+resource "aws_autoscaling_policy" "aws_policy" {
+  name                   = "aws_policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.aws_group.name
+  policy_type = "SimpleScaling"
+}
+
+# CLOUD WATCH MONITORING
+resource "aws_cloudwatch_metric_alarm" "aws_alarm" {
+  alarm_name                = "aws_alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 2
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = 120
+  statistic                 = "Average"
+  threshold                 = 20
+  alarm_description         = "Alarme para monitoração do serviço cloud"
+
+  dimensions = {
+    "AutoScalingGroupName": aws_autoscaling_group.aws_group.name
+  }
+  actions_enabled = true
+  alarm_actions = [aws_autoscaling_policy.aws_police.arn]
+}
+
+# AUTO DESCALING POLICY
+resource "aws_autoscaling_policy" "aws_policy_scaledown" {
+  name                   = "aws_policy_scaledown"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.aws_group.name
+  policy_type = "SimpleScaling"
+}
+
+# DESCALING CLOUD WATCH
+resource "aws_cloudwatch_metric_alarm" "aws_alarm_scaledown" {
+  alarm_name                = "aws_alarm_scaledown"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 2
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = 120
+  statistic                 = "Average"
+  threshold                 = 20
+  alarm_description         = "Alarme para monitoração do serviço de saída da cloud"
+
+  dimensions = {
+    "AutoScalingGroupName": aws_autoscaling_group.aws_group.name
+  }
+  actions_enabled = true
+  alarm_actions = [aws_autoscaling_policy.aws_police.arn]
+}
+
+###############################################################################################################
+
+# LOAD BALANCER
+resource "aws_elb" "elb" {
+}
+
+
+
+
+
+
+
+
