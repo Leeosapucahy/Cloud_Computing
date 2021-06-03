82b32cdbf6016584c7dd5d6762cea73658952c5b
fernandes-mp <83721035+fernandes-mp@users.noreply.github.com>
2021-06-03 20:17:45 -0300

    Add files via upload

---
+++ terraform-aws/security-group.tf
@@ -0,0 +1,99 @@
+resource "aws_security_group" "acesso-ssh-mp" {
+  name        = "rds_sg"
+  description = "rds_sg"
+  vpc_id      = aws_vpc.vpc.id
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.cdirs_acesso_remoto
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mp_leo"
+  }
+}
+
+resource "aws_security_group" "apis" {
+  name   = "apis"
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Colocar o IP do bastion host aqui, só esse
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80 # Rodar API na porta 8000
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306 # Rodar API na porta 8000
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "TLS from VPC"
+    from_port   = 0 # Rodar API na porta 8000
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "mp_leo"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "rds_sg"
+  vpc_id      = aws_vpc.vpc.id
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306 
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.cdirs_acesso_remoto
+
+  }
+
+  egress {
+    description = "TLS from VPC"
+    from_port   = 0 
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "mp_leo"
+  }
+}
