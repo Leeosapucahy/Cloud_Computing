6e07a87177e0812c62bc1fadf6665add7ee47d18
Leosapucahy <126193875+Leeosapucahy@users.noreply.github.com>
2023-02-23 15:47:18 -0300

    Delete Desafio 3 directory

--- Desafio 3/internet_gateway.tf
+++
@@ -1,7 +0,0 @@
-#INTERNET GATEWAY
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "mp_leo"
-  }
-}
--- Desafio 3/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  version = "3.40.0"
-  region  = "us-east-1"
-}
-
-
-
